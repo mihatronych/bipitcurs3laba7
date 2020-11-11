9fc70f06df1bc848c52fe468ac738f032993b1de
mihatronych <dolgushin.mikhail131200@yandex.ru>
2020-11-11 17:05:29 +0500

    7-th labka
    Собсна 7-ая лабка

---
+++ wcf_chat/ChatClient/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.bConnDicon = new System.Windows.Forms.Button();
+            this.lbChat = new System.Windows.Forms.ListBox();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(152, 12);
+            this.tbUserName.Multiline = true;
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(636, 40);
+            this.tbUserName.TabIndex = 0;
+            this.tbUserName.Text = "Имя юзера";
+            // 
+            // bConnDicon
+            // 
+            this.bConnDicon.Location = new System.Drawing.Point(12, 12);
+            this.bConnDicon.Name = "bConnDicon";
+            this.bConnDicon.Size = new System.Drawing.Size(134, 40);
+            this.bConnDicon.TabIndex = 1;
+            this.bConnDicon.Text = "Connect";
+            this.bConnDicon.UseVisualStyleBackColor = true;
+            this.bConnDicon.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbChat
+            // 
+            this.lbChat.FormattingEnabled = true;
+            this.lbChat.ItemHeight = 16;
+            this.lbChat.Location = new System.Drawing.Point(12, 58);
+            this.lbChat.Name = "lbChat";
+            this.lbChat.Size = new System.Drawing.Size(776, 260);
+            this.lbChat.TabIndex = 2;
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(12, 324);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(776, 22);
+            this.tbMessage.TabIndex = 3;
+            this.tbMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbMessage_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.lbChat);
+            this.Controls.Add(this.bConnDicon);
+            this.Controls.Add(this.tbUserName);
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(818, 497);
+            this.MinimumSize = new System.Drawing.Size(818, 497);
+            this.Name = "Form1";
+            this.Text = "ClientChat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Button bConnDicon;
+        private System.Windows.Forms.ListBox lbChat;
+        private System.Windows.Forms.TextBox tbMessage;
+    }
+}
+
---
+++ wcf_chat/ChatClient/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChatClient.ServiceChat;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form , IServiceChatCallback
+    {
+        bool isConnected = false;
+        ServiceChatClient client;
+        int ID;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+                ID = client.Connect(tbUserName.Text);
+                tbUserName.Enabled = false;
+                bConnDicon.Text = "Disconnect";
+                isConnected = true;
+                var str = Assembly.GetExecutingAssembly().Location;
+                var ar = str.Split('\\');
+                str = "";
+                for (int i = 0; i < ar.Length - 4; i++)
+                {
+                    str += ar[i] + "/";
+                }
+                str += "/ChatHost/bin/Debug/history.txt";
+                try
+                {
+                    var lines = File.ReadAllLines(str, Encoding.UTF8);
+                    foreach (var line in lines)
+                    {
+                        lbChat.Items.Add(line);
+                    }
+                    lbChat.SelectedIndex = lbChat.Items.Count - 1;
+                }
+                catch
+                {
+                    MessageBox.Show("Нет файла истории ");
+                }
+            }
+        }
+
+        void DisonnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disconnect(ID);
+                client = null;
+                tbUserName.Enabled = true;
+                bConnDicon.Text = "Connect";
+                isConnected = false;
+                lbChat.Items.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                DisonnectUser();
+            }
+            else
+            {
+                ConnectUser();
+            }
+        }
+
+        public void MsgCallback(string msg)
+        {
+            lbChat.Items.Add(msg);
+            lbChat.SelectedIndex = lbChat.Items.Count - 1;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisonnectUser();
+        }
+
+        private void tbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (client != null)
+                {
+                    client.SendMsg(tbMessage.Text, ID);
+                    tbMessage.Text = string.Empty;
+                }
+            }
+        }
+    }
+}
